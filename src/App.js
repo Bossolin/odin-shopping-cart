8b383316802474cc80578334eb295cc7aedade46
Bossolin <vladyslav.okipnyak@icloud.com>
2022-02-13 01:57:20 +0100

    added basic layout and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">hello</div>;
+  const [showCart, setShowCart] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+          </Routes>
+          {showCart && <Cart />}
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
