09f5ff5b0a737d605d0b4d41c36166b772b0fec4
Bossolin <vladyslav.okipnyak@icloud.com>
2022-02-19 16:05:24 +0100

    added cart order logic

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,16 +9,69 @@ const Cart = ({ setShowCart, cart, setCart }) => {
     setShowCart((prevState) => !prevState);
   };
 
+  const order = [];
+  let orderTotal = 0;
+
+  for (let item in cart) {
+    order.push(cart[item]);
+
+    console.log(cart[item].price, cart[item].amount);
+
+    orderTotal += cart[item].price * cart[item].amount;
+  }
+
+  const decrement = (item) => {
+    const newState = { ...cart };
+
+    newState[item.id].amount -= 1;
+
+    if (newState[item.id].amount === 0) {
+      delete newState[item.id];
+    }
+
+    setCart(newState);
+  };
+
+  const increment = (item) => {
+    const newState = { ...cart };
+
+    newState[item.id].amount += 1;
+
+    setCart(newState);
+  };
+
   return (
     <div className="cart">
       <FontAwesomeIcon icon={faX} onClick={handleClick} />
       <h2>Cart</h2>
-      {cart.length === 0 ? (
+      {order.length === 0 ? (
         <Link to="/shop" onClick={handleClick}>
           Go shop
         </Link>
       ) : (
-        cart.map((item, i) => <div key={i}>{item.title}</div>)
+        <div className="cart-order">
+          <div className="order-items">
+            {order.map((item, i) => (
+              <div key={i}>
+                <img src={item.image} className="order-item-image" />
+                <div className="order-item-title">{item.title}</div>
+                <div className="order-item-price">
+                  {(item.price * item.amount).toFixed(2)} $
+                </div>
+                <div className="order-item-counter">
+                  <button onClick={() => decrement(item)}>-</button>
+                  <p>{item.amount}</p>
+                  <button onClick={() => increment(item)}>+</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="order-total">Total: {orderTotal.toFixed(2)} $</div>
+          <Link to="/shop" onClick={handleClick}>
+            Back to shopping
+          </Link>
+          <button className="order-checkout">Checkout</button>
+        </div>
       )}
     </div>
   );
