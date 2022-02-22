c427f479c2cbcdbff735668d8f01a1ab9eb78685
Bossolin <vladyslav.okipnyak@icloud.com>
2022-02-22 16:09:36 +0100

    finished alpha styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -38,39 +38,45 @@ const Cart = ({ setShowCart, cart, setCart }) => {
   };
 
   return (
-    <div className="cart">
-      <FontAwesomeIcon icon={faX} onClick={handleClick} className="cart-x" />
-      {order.length === 0 ? (
-        <Link to="/shop" onClick={handleClick}>
-          Go shop
-        </Link>
-      ) : (
-        <div className="cart-order">
-          <div className="order-items">
-            {order.map((item, i) => (
-              <div className="order-item" key={i}>
-                <img src={item.image} className="order-item-image" alt="" />
-                <div className="order-info">
-                  <div className="order-item-title">{item.title}</div>
-                  <div className="order-item-price">
-                    {(item.price * item.amount).toFixed(2)} $
-                  </div>
-                  <div className="order-item-counter">
-                    <button onClick={() => decrement(item)}>-</button>
-                    <p>{item.amount}</p>
-                    <button onClick={() => increment(item)}>+</button>
+    <div className="cart-wrapper">
+      <div className="cart-cover" onClick={handleClick}></div>
+      <div className="cart">
+        <FontAwesomeIcon icon={faX} onClick={handleClick} className="cart-x" />
+        {order.length === 0 ? (
+          <div className="cart-empty">
+            <div className="cart-empty-message">Your cart is empty</div>
+            <Link to="/shop" onClick={handleClick} className="cart-empty-back">
+              Go shop
+            </Link>
+          </div>
+        ) : (
+          <div className="cart-order">
+            <div className="order-items">
+              {order.map((item, i) => (
+                <div className="order-item" key={i}>
+                  <img src={item.image} className="order-item-image" alt="" />
+                  <div className="order-info">
+                    <div className="order-item-title">{item.title}</div>
+                    <div className="order-item-price">
+                      {(item.price * item.amount).toFixed(2)} $
+                    </div>
+                    <div className="order-item-counter">
+                      <button onClick={() => decrement(item)}>-</button>
+                      <p>{item.amount}</p>
+                      <button onClick={() => increment(item)}>+</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="order-total">Total: {orderTotal.toFixed(2)} $</div>
+            <Link to="/shop" onClick={handleClick} className="order-back">
+              Back to shopping
+            </Link>
+            <button className="order-checkout">Checkout</button>
           </div>
-          <div className="order-total">Total: {orderTotal.toFixed(2)} $</div>
-          <Link to="/shop" onClick={handleClick}>
-            Back to shopping
-          </Link>
-          <button className="order-checkout">Checkout</button>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,13 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <p>
+      <h1 className="home-title">Online Shop</h1>
+      <p className="home-body">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, libero
         atque totam fugit veritatis ipsum sint volupttem ea, odio delectus non
         autem porro distinctio ab? Error facilis corrupti autem debitis!
       </p>
-      <Link to="/shop" className="browse">
+      <Link to="/shop" className="home-browse">
         Browse
       </Link>
     </div>
